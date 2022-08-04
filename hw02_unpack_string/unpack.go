db5df58a3840d348f03e87334b456c475424cf6a
danko_a <a.danko@abr.ru>
2022-08-04 07:55:53 +0300

    update version golangci-lint fix code

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -13,12 +13,10 @@ func Unpack(s string) (string, error) {
 	if s == "" {
 		return "", nil
 	}
-
 	sRunes := []rune(s)
 	if unicode.IsDigit(sRunes[0]) {
 		return "", ErrInvalidString
 	}
-
 	var b strings.Builder
 	for i, rCount := 0, len(sRunes); i < rCount; i++ {
 		r := sRunes[i]
@@ -27,7 +25,6 @@ func Unpack(s string) (string, error) {
 			if i == rCount-1 {
 				return "", ErrInvalidString
 			}
-
 			if i < rCount-2 && unicode.IsDigit(sRunes[i+2]) {
 				tmp, err := strconv.Atoi(string(sRunes[i+2]))
 				if err != nil {
@@ -35,32 +32,25 @@ func Unpack(s string) (string, error) {
 				}
 				b.WriteString(strings.Repeat(string(sRunes[i+1]), tmp))
 				i += 2
-
 				continue
 			}
 			b.WriteRune(sRunes[i+1])
 			i++
-
 			continue
 		}
-
 		if unicode.IsDigit(r) {
 			if i < rCount-1 && unicode.IsDigit(sRunes[i+1]) {
 				return "", ErrInvalidString
 			}
-
 			continue
 		}
-
 		if i < rCount-1 && unicode.IsDigit(sRunes[i+1]) {
 			tmp, _ := strconv.Atoi(string(sRunes[i+1]))
 			b.WriteString(strings.Repeat(string(r), tmp))
 
 			continue
 		}
-
 		b.WriteRune(r)
 	}
-
 	return b.String(), nil
 }
